8702bd91881b04850de76aabc0c2e3b8f5672dc8
ADWAITH B S <adwaithbspaika@gmail.com>
2026-01-31 15:28:31 +0530

    Add files via upload

---
+++ string.c
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> str:
+        from typing import List 
+class Solution: 
+    def reverseString(self, s: List[str]) -> None: 
+        s[:] = s[::-1]
+       
+        
